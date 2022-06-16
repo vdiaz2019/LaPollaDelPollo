86f55db9d3f18a7af9e82cfb3ab7996646faef0c
Victor Diaz <vhdiazro@gmail.com>
2022-06-16 17:46:29 -0500

    Creación del proyecto

---
+++ LaPollaDelPollo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LaPollaDelPollo
+//
+//  Created by Victor Hugo Diaz Rojas on 16/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
